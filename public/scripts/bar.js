2451a3d9ff27e2b01e7d15843341ea62e3010d51
Amir m <amoin@langara.ca>
2023-12-07 11:40:32 -0800

    tried generating lines

--- public/scripts/bar.js
+++ public/scripts/bar.js
@@ -1,5 +1,5 @@
 import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
-import { Location } from "./Location.js"
+import { Location } from "./location.js"
 import { Options } from "./options.js"
 
 
